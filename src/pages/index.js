a7401c44835f6a6f773470f7d049afcaf0e5fc55
Shubham Takankhar <takankhar@gmail.com>
2021-07-29 15:07:07 +0530

    authenticate bing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,14 @@ import React from "react"
 import Seo from "./../components/seo"
 import Layout from "./../components/layout"
 import About from "../components/about"
+import { Helmet } from "react-helmet"
 
 const IndexPage = () => (
     <Layout> 
+      <Helmet>  
       <meta name="msvalidate.01" content="44E73AAC2392D270C238935BB9028F82" />
-      <Seo title = "About me" / >
+      </Helmet>
+      <Seo title = "About me" />
       <About /> 
     </Layout>
   )
